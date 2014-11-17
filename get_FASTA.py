a7df9265d3535838c855f85d8a45a8d884a80c41
Josh Rosen <jdrosen@ucdavis.edu>
2014-11-16 16:56:40 -0800

    Reads in DNA for Rosalind.info problems
    Added rosalind data file

---
+++ get_FASTA.py
@@ -0,0 +1,27 @@
+#Authot: Josh Rosen
+#This script recieves a FASTA file and returns the corresponding FASTA names and DNA
+#sequences into a dictionary named rosDict
+
+def main():
+    #Opens FASTA file
+    workfile = input("Enter FASTA file name: ")
+    f = open(workfile, 'r')
+    
+    #Creates dictionary
+    rosDict={}
+    
+    #Parse over FASTA file
+    for line in f:
+        
+        #Checks for FASTA name and puts it into dictionary
+        if line[0]=='>':
+            rosDict[line.rstrip('\n')]=''
+            index = line.rstrip('\n')
+            
+        #Inputs DNA sequence to corresponding FASTA name based on index
+        else:
+            rosDict[index] = rosDict.get(index)+line.rstrip('\n')
+            
+    #Returns Dictionary of FASTA names and corresponding DNA sequences
+    return rosDict
+
